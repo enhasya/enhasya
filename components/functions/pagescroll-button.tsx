12b5387d095130e27cefe4e24cd7cf2c2d05f4ee
enhasya <enhasya@outlook.co.id>
2024-03-14 09:57:34 +0700

    update

--- components/functions/pagescroll-button.tsx
+++ components/functions/pagescroll-button.tsx
@@ -15,10 +15,10 @@ const FunctionPageScrollButton: React.FC<PageScrollButtonProps> = ({
   return (
     <button
       onClick={onClick}
-      className="bg-[#0D0300] ring-1 ring-[#190600] transition-all hover:bg-[#190600] hover:ring-[#260900] flex flex-row gap-2 items-center w-full lg:w-auto h-auto rounded-full px-4 py-2"
+      className="bg-[#0D0300] ring-1 ring-[#190600] transition-all hover:bg-[#190600] hover:ring-[#260900] flex flex-row gap-2 items-center justify-center lg:justify-start w-full lg:w-auto h-auto rounded-full px-4 py-2"
     >
       <Image src={icon} width="14" height="14" alt="image: Icon" />
-      <p className="text-[#FF3D00] font-medium text-xs text-start tracking-tight leading-relaxed mt-0">
+      <p className="hidden lg:flex text-[#FF3D00] font-medium text-xs text-start tracking-tight leading-relaxed mt-0">
         {title}
       </p>
     </button>
