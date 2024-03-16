fff2877f5efb9251461bf46975f15124f0d09ab5
enhasya <enhasya@outlook.co.id>
2024-03-16 20:03:24 +0700

    update

--- components/contents/landing/profile.tsx
+++ components/contents/landing/profile.tsx
@@ -20,7 +20,7 @@ const LandingProfile = () => {
 
   return (
     <div className="relative flex flex-col w-full h-auto items-start lg:items-center justify-center px-8 pt-0 pb-8 lg:px-32 lg:pt-0 lg:pb-32">
-      <div className="bg-[#0C0C0C] ring-1 ring-[#181818] flex flex-col lg:flex-row w-full lg:w-[50%] h-auto items-start justify-center gap-0 lg:gap-16 rounded-lg p-8 lg:p-16">
+      <div className="bg-[#0C0C0C] ring-1 ring-[#181818] flex flex-col lg:flex-row w-full lg:w-[720px] h-auto items-start justify-center gap-0 lg:gap-16 rounded-lg p-8 lg:p-16">
         <div className="flex flex-row lg:flex-wrap gap-4 items-center justify-start w-full h-auto mt-0">
           <FunctionPageScrollButton
             onClick={() => setActiveSession(1)}
